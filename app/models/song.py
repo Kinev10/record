7b8236aecfb4319b860a0458f55800fef406e4be
Frank <fbablao@up.edu.ph>
2021-11-23 22:41:57 +0800

    added model Song

--- app/models/song.py
+++ app/models/song.py
@@ -5,7 +5,7 @@ from django.utils.translation import ugettext_lazy as _
 class Song(models.Model):
     """Model representing a song."""
     title = models.CharField(max_length=200,)
-    duration = models.DurationField(_("Duration"))
+    duration = models.FloatField(_("Duration"))
     writer = models.ManyToManyField("app.Artist", verbose_name=_("Writer"))
 
     class Meta:
